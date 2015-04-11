cc12dc2ae54ba752de3e0d0435f3728d86ce5acd
CyrusRoshan <CyrusRoshan@users.noreply.github.com>
2015-04-11 12:46:30 -0500

    trying to use js to evenly space nav bar elements regardless of count or width

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 $(window).load(function() {
     // DOM modifying stuff goes here:
-
+	$(".navLink").css("line-height", "1");
+	window.navLinkHeight = $(".navLink").css("line-height");
+	window.htmlWidth = $("html").width();
+	var numLinks = $(".navLink").length;
+	var height = parseInt(navLinkHeight);
+	var width = $(".navLink").width();
+	
+	var totalWidth = 0;
+	$(".navLink").each(function(index) {
+		totalWidth += parseInt($(this).width(), 10);
+	});
+	
+	$(".navLink").css({
+		paddingLeft: height + "px",
+		paddingRight: height + "px",
+		paddingTop: height + "px",
+		paddingBottom: height + "px",
+		marginLeft: (((.75 * htmlWidth) - totalWidth) / numLinks - (2 * height)) / 2 + "px",
+		marginRight: (((.75 * htmlWidth) - totalWidth) / numLinks - (2 * height)) / 2 + "px"
+	});
 });
